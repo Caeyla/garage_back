41a80fcc32456d88845d227fe6eda22c0887d650
caeyla <mortelcail@gmail.com>
2025-03-07 00:16:16 +0300

    user adapter  update

--- adapters/UserMongoAdapter.js
+++ adapters/UserMongoAdapter.js
@@ -7,6 +7,19 @@ const UserSchema = new mongoose.Schema({
 });
 
 class UserMongoAdapter  {
+  constructor() {
+    this.UserModel = mongoose.model('User', UserSchema);
+  }
+
+  async create(user) {
+    const newUser = new this.UserModel(user);
+    await newUser.save();
+  }
+
+  async findByName(name) {
+    const user = await this.UserModel.findOne({ name });
+    return new User(user.name, user.password);
+  }
    
 }
 
