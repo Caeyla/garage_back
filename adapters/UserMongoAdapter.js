d95d02b0309e616b73bbd536d5f2f401f9f521c1
caeyla <mortelcail@gmail.com>
2025-03-07 17:28:25 +0300

    add workflows for main

--- adapters/UserMongoAdapter.js
+++ adapters/UserMongoAdapter.js
@@ -7,7 +7,23 @@ const UserSchema = new mongoose.Schema({
 });
 
 class UserMongoAdapter  {
-   
+  constructor() {
+    this.model = mongoose.model('User', UserSchema);
+  }
+
+  async create(user) {
+    const newUser = new this.model(user);
+    await newUser.save();
+  }
+
+  async findByName(name) {
+    const user = await this.model.findOne({ name });
+    return new User(user.name, user.password);
+  }
+
+  async deleteAll() {
+    await this.model.deleteMany({});
+  }
 }
 
 module.exports = UserMongoAdapter;
