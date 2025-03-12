34ad9a774c4db194763a3412245f0e0f66a392d2
caeyla <mortelcail@gmail.com>
2025-03-12 22:29:24 +0300

    add health check route

--- config/RouteConfig.js
+++ config/RouteConfig.js
@@ -3,6 +3,7 @@ initRoutes  = (app) => {
     app.use('/customers', require('../routes/CustomerRoutes'));
     app.use('/manager', require('../routes/ManagerRoutes'));
     app.use('/user', require('../routes/UserRoutes'));
+    app.use('/health', require('../routes/HealthRoute'));
 }
 
 module.exports = initRoutes;
